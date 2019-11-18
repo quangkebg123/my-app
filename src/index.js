3bddd42b65e16f340883ec14c8b9a5390694d038
Nguyen Manh Quang <quangkebg123@gmail.com>
2019-11-18 07:20:50 +0700

    a

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/* eslint-disable no-unused-vars */
+import ReactDOM from 'react-dom'
+import React from 'react'
+import './index.css'
+// eslint-disable-next-line no-unused-vars
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import 'bootstrap/dist/css/bootstrap.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
